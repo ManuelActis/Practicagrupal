d4690c40ee6b679e73e576f71ded2c7e5e926d9e
nicoram28 <nahuelramos.marset@gmail.com>
2021-05-20 13:50:44 -0300

    lo ultimo

--- JavaTest/src/JavaTest.java
+++ JavaTest/src/JavaTest.java
@@ -2,6 +2,6 @@ public class JavaTest {
 
     public static void main(String[] args) {
         System.out.println("Hello Word");
-        System.out.println("###### GRUPO 7 ######");
+        System.out.println("\n###### GRUPO 7 ######\n");
     }
 }
