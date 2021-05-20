1c5f316413866428d89304b3ecb942984d240cb6
Brandon Russell <brandon.russell@mercadolibre.com>
2021-05-20 13:42:14 -0300

    This is a conflict commit

--- JavaTest/src/JavaTest.java
+++ JavaTest/src/JavaTest.java
@@ -2,6 +2,6 @@ public class JavaTest {
 
     public static void main(String[] args) {
         System.out.println("Hello Word");
-        System.out.println("AJSHDASHDJAS");
+        System.out.println("This is my line with conflict!");
     }
 }
